809641b4792ec326f1ccc1e8f022c66760758614
Mateusz Falkowski <mateusz.falkowski@cgi.com>
2018-05-10 14:36:10 +0200

    Step 2 - fruit manager component

---
+++ src/Fruits/FruitsManager.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FruitsManager.css';
+
+class FruitsManager extends React.Component {
+  render () {
+    return (
+      <div className="fruits-manager">
+        Welcome to Fruits Manager
+      </div>
+    )
+  }
+}
+
+export default FruitsManager;
